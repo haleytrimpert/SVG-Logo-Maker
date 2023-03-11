24ad62c6e6d3128ad2a550536b0b977a6154c663
Haley T <hntrimpert@yahoo.com>
2023-03-11 13:49:36 -0500

    index init function

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const questions = [""];
+const questions = ["Enter the text for your logo. Max 3 characters.",
+"What text color would you like?",
+"What shape would you like your logo to be?",
+"What color would you like your shape to be?"];
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync('Logo', fileName)
+}
+
+function init() {
+    inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: questions[0]
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: questions[1]
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: questions[2],
+        choices: ["circle", "triangle", "square"]
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: questions[3]
+    }
+  ])
+}
